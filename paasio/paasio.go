a28367f15f1c454d3a3b94d9bf4b7d2b6cec07d3
Mofizur Rahman <moficodes@gmail.com>
2019-02-25 01:20:03 -0500

    added paasio code

---
+++ paasio/paasio.go
@@ -0,0 +1,75 @@
+package paasio
+
+import (
+	"io"
+	"sync"
+)
+
+type counter struct {
+	sync.Mutex
+	n    int64
+	nops int
+}
+
+type readCounter struct {
+	r io.Reader
+	counter
+}
+
+type writeCounter struct {
+	w io.Writer
+	counter
+}
+
+type readWriteCounter struct {
+	ReadCounter
+	WriteCounter
+}
+
+func (c *counter) count() (int64, int) {
+	c.Lock()
+	defer c.Unlock()
+	return c.n, c.nops
+}
+
+func (c *counter) addBytes(n int) {
+	c.Lock()
+	defer c.Unlock()
+	c.n += int64(n)
+	c.nops++
+}
+
+func (rc *readCounter) Read(p []byte) (int, error) {
+	n, err := rc.r.Read(p)
+	rc.addBytes(n)
+	return n, err
+}
+
+func (rc *readCounter) ReadCount() (int64, int) {
+	return rc.count()
+}
+
+func (wc *writeCounter) Write(p []byte) (int, error) {
+	n, err := wc.w.Write(p)
+	wc.addBytes(n)
+	return n, err
+}
+
+func (wc *writeCounter) WriteCount() (int64, int) {
+	return wc.count()
+}
+
+// NewReadCounter takes a reader and returns a ReadCounter
+func NewReadCounter(r io.Reader) ReadCounter {
+	return &readCounter{r, counter{}}
+}
+
+// NewWriteCounter takes a writer and returns a WriteCounter
+func NewWriteCounter(w io.Writer) WriteCounter {
+	return &writeCounter{w, counter{}}
+}
+
+//NewReadWriteCounter takes a readerwriter and returns a ReadWriteCounter
+func NewReadWriteCounter(rw io.ReadWriter) ReadWriteCounter {
+	return &readWriteCounter{NewReadCounter(rw), NewWriteCounter(rw)}
+}
